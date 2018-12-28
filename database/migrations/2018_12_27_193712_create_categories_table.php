5d80b84a4307358f9820e693fbb705161c425414
RedLionHeart <samura95@gmail.com>
2018-12-28 12:48:27 +0300

    first commit

---
+++ database/migrations/2018_12_27_193712_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('category_name');
+            $table->integer('category_id');
+            $table->text('maker_name');
+            $table->integer('maker_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
